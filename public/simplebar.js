b7763da08df3e0f30bf480ce153dcbcf106639e1
David Fagerlund <davidfagerlund@outlook.com>
2016-02-09 11:41:49 +0100

    Created bar chart with a index

--- public/simplebar.js
+++ public/simplebar.js
@@ -1,62 +1,53 @@
 /* global generateColors */
 /* global d3 */
+var margin = {top: 30, right: 40, bottom: 30, left: 150};
+var canvas=d3.select('body').append('svg')
+    .attr('width', '95%')
+    .attr('height', '95%')
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+var width=parseInt(d3.select("svg").style('width'), 10)
+            - margin.left - margin.right;
+var height=parseInt(d3.select("svg").style('height'), 10)
+- margin.left - margin.right;
+var xScale=d3.scale.linear();
+xScale.rangeRound([0, width-margin.right]);
+var yScale=d3.scale.ordinal()
+    .rangeRoundBands([0, height], 0.1)
+// var questions=Object.keys(countries[0].values).filter((d)=>{ return (d!='(N)' && d!="Don´t know")});
+var questions=['Increased private ownership', '2', '3', '4', '5', '6', '7', '8', '9', 'Increased state ownership'];
+var years=['2010-2014', '2005-2009', '1999-2004', '1994-1998', '1989-1993'];
+var countries=null;
+var colors=null;
 
-d3.json('data/dataFive.json', (error, countries)=>{
+d3.json('data/dataFive.json', (error, data)=>{
+    countries=data;
     d3.select(window).on('resize', resize);
     console.log(countries);
-    var margin = {top: 30, right: 40, bottom: 30, left: 150};
-    var canvas=d3.select('body').append('svg')
-        .attr('width', '95%')
-        .attr('height', '95%')
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    var width=parseInt(d3.select("svg").style('width'), 10)
-                - margin.left - margin.right;
-    var height=parseInt(d3.select("svg").style('height'), 10)
-    - margin.left - margin.right;
-    
-    // var questions=Object.keys(countries[0].values).filter((d)=>{ return (d!='(N)' && d!="Don´t know")});
-    var questions=['Increased private ownership', '2', '3', '4', '5', '6', '7', '8', '9', 'Increased state ownership'];
     // console.log(questions);
-    var colors=generateColors(countries.map((d)=>{return d.name;}));
+    var countryNames=countries.map((d)=>{return d.name;});
+    colors=generateColors(countryNames);
     console.log(colors);
     
-    var change=[];
-    questions.forEach((d)=>{
-        var x0=0;
-        var bar={};
-        bar.name=d;
-        bar.countries=countries.map((c)=>{
-            // console.log((c.values[d]["2010-2014"]).toString()+' | '+(c.values[d]["2005-2009"]).toString());
-            return {country: c.name, x0: x0, x1: x0+=((+c.values[d]["2010-2014"]))};//-(+c.values[d]["2005-2009"])
-        });
-        bar.total=bar.countries[bar.countries.length-1].x1;
-        change.push(bar);
-    });
-    console.log([0, d3.max(change, (d)=>{ return d.total;})]);
-    var xScale=d3.scale.linear();
-    xScale.rangeRound([0, width-margin.right]);
-    xScale.domain([0, d3.max(change, (d)=>{ return d.total;})]);
-    var yScale=d3.scale.ordinal()
-        .rangeRoundBands([0, height], 0.1)
-        .domain(questions);
+    var change=createIndex(countries, years[0]);
+    console.log([0, d3.max(change, (d)=>{ return d.index;})]);
     
-    var que=canvas.append('g')
+    xScale.domain([0, d3.max(change, (d)=>{ return d.index;})]);
+    yScale.domain(countryNames);
+    
+    canvas.append('g')
         .selectAll('.questions')
         .data(change)
         .enter()
         .append('g')
         .attr('class', '.questions')
-        .attr('transform', (d)=>{ return "translate(0,"+yScale(d.name)+")" });
-        
-    que.selectAll('rect')
-        .data((d)=>{ return d.countries;})
-        .enter()
+        .attr('transform', (d)=>{ return "translate(0,"+yScale(d.name)+")" })
         .append('rect')
-        .attr('width', (d)=>{ return xScale(Math.abs(d.x1))-xScale(Math.abs(d.x0));})
+        .attr('class', 'countryindex')
+        .attr('width', (d)=>{ return xScale(d.index);})
         .attr('height', yScale.rangeBand())
-        .attr('x', (d)=>{ return xScale(d.x0); })
-        .style('fill', (d)=>{ return colors[d.country]; });
+        .attr('x', (d)=>{ return 0; })
+        .style('fill', (d)=>{ return colors[d.name]; });
     //console.log(xScale(d.x1).toString()+' '+xScale(d.x0).toString()+' '+d.x1.toString()+' '+d.x0.toString());
     var xAxis = d3.svg.axis()
     .scale(xScale)
@@ -111,3 +102,25 @@ d3.json('data/dataFive.json', (error, countries)=>{
         //     .attr('width', (d, i)=>{ return 50; })
     }
 });
+
+function createIndex(countries, year){
+    return countries.map((c)=>{
+                var i=questions.reduce((prev, d, i)=>{ return prev+=((i+1)*(+c.values[d][year])); }, 0)
+                return {name: c.name, index: i};
+            });
+}
+
+function updatechart(obj){
+    if(countries===null)
+        return;
+        
+    console.log();
+    var change=createIndex(countries, years[obj.value]);
+    xScale.domain([0, d3.max(change, (d)=>{ return d.index;})]);
+    canvas.selectAll('.countryindex')
+        .data(change)
+        .transition()
+        .duration(1000)
+        .attr('width', (d)=>{ return isNaN(d.index)?xScale(xScale.domain()[1]):xScale(d.index);})
+        .style('fill', (d)=>{ return isNaN(d.index)?'black':colors[d.name]; });
+}
